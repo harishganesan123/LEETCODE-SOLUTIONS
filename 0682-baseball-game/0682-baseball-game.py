fedbe8d4c9ec43bb62a780f7e9a392bf637c4e9c
Harish G <harishganesan123@gmail.com>
2026-06-25 12:58:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (41.21%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stk =[]
+        for i in operations:
+            if i == '+':
+                stk.append(stk[-1]+stk[-2])
+            elif i=='D':
+                stk.append(2*stk[-1])
+            elif i=='C':
+                stk.pop()
+            else:
+                stk.append(int(i))
+        return sum(stk)
+        
